4f11c97de707780cae6b5be3e1e80db716eaa02e
Shawn <shawnharrigan08@icloud.com>
2024-08-20 21:36:05 -0400

    Tried my best :3 mostly looks fine for mobile calendar needs work though :(

--- src/Components/CalendarComponent.jsx
+++ src/Components/CalendarComponent.jsx
@@ -12,6 +12,8 @@ const CalendarApp = () => {
       events={{
         googleCalendarId: 'cf24533a5a742a776862c66fe6eedfd1ccb9bcbdee195a6c82ec3367fd2cbc2d@group.calendar.google.com', 
       }}
+        aspectRatio={2} 
+        contentHeight="auto"
     />
     </div>
   );
--- src/Routes/Aboutus.jsx
+++ src/Routes/Aboutus.jsx
@@ -13,7 +13,7 @@ export default function Aboutus(){
     return (
         <div className={`About-Us-Container ${cursorStyle}`} data-theme={isdark ? "dark" : "light"}>
             <Toggle /> 
-            <h1 className='page-header'>We are team PROTA 172<button id='secretbutton'onClick={() => changeCursor()}>2</button>2</h1>
+            <h1 className='page-header'>We are team PROTA 172<h1 className='mobile-replace-button'>2</h1><button id='secretbutton'onClick={() => changeCursor()}>2</button>2</h1>
             <h2 id='page-subheader'>We are a First Tech Challenge team formed in 2019 we have since competed in various events</h2>
             <h3 className='list-container'>Events Competed in</h3>
             <ul className='list'>
